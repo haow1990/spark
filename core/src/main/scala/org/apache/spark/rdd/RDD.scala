cea647a192db8e04d8dd7b242fb500bbace66e21
Hao Wang <haow1990@gmail.com>
2015-04-10 14:28:41 +0800

    Add detialed RDD, stage and job output

--- core/src/main/scala/org/apache/spark/rdd/RDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/RDD.scala
@@ -125,6 +125,9 @@ abstract class RDD[T: ClassTag](
   /** A unique ID for this RDD (within its SparkContext). */
   val id: Int = sc.newRddId()
 
+  private val _printStacktrace =
+    logInfo(s"HAO-RDD $id ${Thread.currentThread.getStackTrace.drop(2).map(_.toString).mkString(" @from ")}")
+
   /** A friendly name for this RDD */
   @transient var name: String = null
 
